ddfd067786205a68965e9fed2295b2498c16e016
Manuel Odendahl <wesen@ruinwesen.com>
2024-08-06 12:45:55 -0400

    :sparkles: Add autocomplete TUI mode to prompto

--- cmd/prompto/cmds/get.go
+++ cmd/prompto/cmds/get.go
@@ -2,20 +2,21 @@ package cmds
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/charmbracelet/huh"
 	files2 "github.com/go-go-golems/glazed/pkg/helpers/files"
 	"github.com/go-go-golems/prompto/pkg"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 func NewGetCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "get prompt [args]",
 		Short: "Get and execute or print a file",
-		Args:  cobra.MinimumNArgs(1),
 		RunE:  get,
 	}
 
@@ -24,7 +25,73 @@ func NewGetCommand() *cobra.Command {
 	return cmd
 }
 
+func interactiveGet() error {
+	repositories := viper.GetStringSlice("repositories")
+	var allFiles []pkg.FileInfo
+	var selectedPrompt string
+	var searchTerm string
+
+	// Gather all files from repositories
+	for _, repo := range repositories {
+		files, err := pkg.GetFilesFromRepo(repo)
+		if err != nil {
+			return err
+		}
+		allFiles = append(allFiles, files...)
+	}
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Search for a prompt").
+				Value(&searchTerm),
+			huh.NewSelect[string]().
+				Title("Select a prompt").
+				Value(&selectedPrompt).
+				OptionsFunc(func() []huh.Option[string] {
+					var filteredOptions []huh.Option[string]
+					for _, file := range allFiles {
+						if strings.Contains(strings.ToLower(file.Name), strings.ToLower(searchTerm)) {
+							filteredOptions = append(filteredOptions, huh.NewOption(file.Name, file.Name))
+						}
+					}
+					return filteredOptions
+				}, &searchTerm),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		return err
+	}
+
+	if selectedPrompt == "" {
+		return fmt.Errorf("no prompt selected")
+	}
+
+	// Find the selected prompt in the already gathered files
+	for _, file := range allFiles {
+		if file.Name == selectedPrompt {
+			// Find the repository for this file
+			for _, repo := range repositories {
+				s, err := pkg.RenderFile(repo, file, []string{})
+				if err == nil {
+					fmt.Println(s)
+					return nil
+				}
+			}
+			return fmt.Errorf("error rendering selected prompt")
+		}
+	}
+
+	return fmt.Errorf("selected prompt not found")
+}
+
 func get(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return interactiveGet()
+	}
+
 	prompt := args[0]
 	restArgs := args[1:]
 
